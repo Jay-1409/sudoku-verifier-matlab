429ab9c1b3f18cd39413b089c64e09d86ad91f0e
Jay-1409 <jay_shah@outlook.in>
2024-11-17 14:31:21 +0530

    flowchart + report + enhancements

--- run_sudoku.m
+++ run_sudoku.m
@@ -3,7 +3,7 @@ fprintf('Enter your 9x9 Sudoku grid:\n');
     grid = zeros(9, 9);  
     
     for i = 1:9
-        rowInput = input(sprintf(''), 's');  % Take row as a string
+        rowInput = input("", 's');  % Take row as a string
         grid(i, :) = str2num(rowInput);  % Convert string to numeric row
     end
 sudoku_verifier(grid);
